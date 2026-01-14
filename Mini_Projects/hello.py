bf534b56d8deacf3d1a09f7fbba4e3a446188c55
Haseeb <haseebxpy@gmail.com>
2026-01-14 22:38:45 +0500

    basic refresher

--- Mini_Projects/hello.py
+++ Mini_Projects/hello.py
@@ -1,1 +1,20 @@
-print("hell world")
+print("HELLO WORLD!")
+def is_even(a):
+    if a%2==0:
+        return True
+    else:
+        return False
+name="Haseeb"
+age=25
+person ={
+    "name":"HASEEB"
+}
+print(name)
+print(is_even(4))
+skill=["python","automation","git"]
+for i in range(0,len(skill)):
+    print("skill:",skill[i])
+if age >=18:
+    print("adult")
+else:
+    print("young")
